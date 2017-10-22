03d50fe2f640688a37ca1768b36995c47005f893
Kevin Hannigan <kph1958@rit.edu>
2017-10-22 14:02:22 -0400

    Okay, i think the setup and update are working right now. Only vim is configured to be set up atm

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-set -e -x
+#set -e -x
+
+if [ "$1" != "--update" ] ; then
+    # If run without --update, setup the cron job
+    echo "You need to setup crontab manually. To do so, add the following entry:"
+    echo
+    echo "0 * * * * $SCRIPT_DIR/update.sh"
+    echo
+    echo "Which will check for updates every hour on the hour"
+    echo "--- Press any key to continue ---"
+    read
+    crontab -e
+fi
 
 # Vim
-./$SCRIPT_DIR/vimrc/setup.sh
+$SCRIPT_DIR/vim/setup.sh
--- update.sh
+++ update.sh
@@ -2,5 +2,14 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e -x
 
+# Check if there are changes
+git fetch origin
+reslog=$(git log HEAD..origin/master --oneline)
+if [[ "${reslog}" == ""  ]] ; then
+    echo "No changes"
+    exit 0
+fi
+
+# Pull and run setup again
 git pull
-./$SCRIPT_DIR/setup.sh
+$SCRIPT_DIR/setup.sh
