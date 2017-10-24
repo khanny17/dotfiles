8fb7cb96dc1bf6ba164e4946ec86f5d51d0787b1
Kevin <kph1958@rit.edu>
2017-10-24 13:26:30 -0400

    No more exit on failure

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-set -e -x 
+set -x
 
 # First, check if vim 8 is installed
 VIMVERSION=$(vim --version | head -1 | cut -d ' ' -f 5) # get installed version
