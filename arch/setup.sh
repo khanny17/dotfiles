0529981e83e034d76f157e6c6c050575f244b43b
Kevin <kph1958@rit.edu>
2017-11-03 04:29:13 -0400

    Adding arch and bspwm configs

---
+++ arch/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -x
+
+# FIRST: packages
+sudo pacman -S vim git python3
+
+# NEXT: display stuff
+
+mkdir -p ~/.config/bspwm
+mv ~/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc.old
+ln -s $SCRIPT_DIR/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+
+mkdir -p ~/.config/sxhkd
+mv ~/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc.old
+ln -s $SCRIPT_DIR/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+mv ~/.xinitrc .xinitrc.old
+ln -s $SCRIPT_DIR/xinitrc ~/.xinitrc
+
+mv ~/.Xdefaults .Xdefaults.old
+ln -s $SCRIPT_DIR/Xdefaults ~/.Xdefaults
--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-set -e -x
+set -x
 
 mv ~/.tmux.conf ~/.tmux.conf_old
 ln -s $SCRIPT_DIR/tmux.conf ~/.tmux.conf
