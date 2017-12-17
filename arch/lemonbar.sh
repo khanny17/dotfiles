ea8122e0cbf16a9d0ab1fdd4e700d08b323f045a
Kevin Hannigan <kph1958@rit.edu>
2017-12-16 19:29:43 -0500

    more lemonbar config

--- arch/lemonbar.sh
+++ arch/lemonbar.sh
@@ -8,22 +8,41 @@ __cleanup() {
 trap __cleanup PIPE
 
 clock() {
-    date '+%b %d %l:%M:%S %p'
+    out=$(date '+%b %d %l:%M:%S %p')
+    echo "\uf017 $out"
 }
 
 wifi() {
     ssid=$(iw wlp58s0 link | grep SSID)
     if [ -z "$ssid" ] ; then
-        echo "\uf00d %{O2} rip"
+        echo "\uf00d %{O1} rip"
     else
-        echo "\uf1eb %{O2}"
+        echo "\uf1eb %{O1}"
         echo "$ssid" | sed -e "s/.*SSID://"
+    fi
+}
+
+battery() {
+    cap=$(cat /sys/class/power_supply/BAT0/capacity)
+    if [ "$?" -eq "0" ] ; then
+        # Cool, we got a value
+        if [ "$cap" -gt "75" ] ; then
+            echo "\uf240"
+        elif [ "$cap" -gt "50" ] ; then
+            echo "\uf241"
+        elif [ "$cap" -gt "25" ] ; then
+            echo "\uf242"
+        elif [ "$cap" -gt "10" ] ; then
+            echo "\uf243"
+        else
+            echo "\uf244"
+        fi
     fi
 }
 
 while :; do
     o=""
-    o="%{l} \uf017 $(clock) %{O50} %{r} $(wifi)"
+    o="%{l} $(clock) %{O10} %{r} $(wifi) %{O5} $(battery)"
     echo -e $o
     sleep 1
 done
